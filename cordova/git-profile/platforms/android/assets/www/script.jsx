3f41d3ce0c2fb162082e616f0e37b2e96eea8dc3
satvik <satvik.jagannath7@gmail.com>
2015-10-05 19:57:11 +0530

    Cordova project for Android

---
+++ cordova/git-profile/platforms/android/assets/www/script.jsx
@@ -0,0 +1,60 @@
+var Card = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    var component = this;
+    $.get("https://api.github.com/users/" + this.props.login, function(data) {
+      component.setState(data);
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <img src={this.state.avatar_url} width="80" />
+        <h3>{this.state.name}</h3>
+        <hr/>
+      </div>
+    );
+  }
+});
+
+var Form = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var loginInput = React.findDOMNode(this.refs.login);
+    this.props.addCard(loginInput.value);
+    loginInput.value = '';
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="github login" ref="login" />
+        <button>Add</button>
+      </form>
+    );
+  }
+});
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {logins: []};
+  },
+  addCard: function(loginToAdd) {
+    this.setState({logins: this.state.logins.concat(loginToAdd)});
+  },
+  render: function() {
+    var cards = this.state.logins.map(function(login) {
+      return (<Card login={login} />);
+    });
+    return (
+      <div>
+        <Form addCard={this.addCard} />
+        <hr />
+        {cards}
+      </div>
+    )
+  }
+});
+
+React.render(<Main />, document.getElementById("root"));
